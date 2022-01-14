3af0bdb20226c771a220e7d0716822078b75d905
mdedeu <mdedeu@itba.edu.ar>
2022-01-14 14:58:02 -0300

    Indentation

--- contracts/ETHPool.sol
+++ contracts/ETHPool.sol
@@ -4,9 +4,7 @@ pragma solidity ^0.8.7;
 
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
-
 contract ETHPool is AccessControl {
-
     bytes32 public constant TEAM_MEMBER = keccak256("TEAM_MEMBER");
 
     constructor() {
@@ -15,29 +13,30 @@ contract ETHPool is AccessControl {
 
     address[] stakeHolders;
     mapping(address => bool) exists;
-    mapping(address => uint) balances;
-    uint256 totalBalance; 
+    mapping(address => uint256) balances;
+    uint256 totalBalance;
 
-    function depositRewards() public  onlyRole(TEAM_MEMBER) payable{
-    require(totalBalance > 0);
-    for(uint i= 0; i < stakeHolders.length; i++){
-          balances[stakeHolders[i]] += (msg.value * balances[stakeHolders[i]]  / totalBalance); 
+    function depositRewards() public payable onlyRole(TEAM_MEMBER) {
+        require(totalBalance > 0);
+        for (uint256 i = 0; i < stakeHolders.length; i++) {
+            balances[stakeHolders[i]] += ((msg.value * balances[stakeHolders[i]]) / totalBalance);
+        }
+        totalBalance += msg.value;
     }
-      totalBalance += msg.value;
-    }
-    function withdraw() public{
-        require(balances[msg.sender] > 0 );
-        uint amount = balances[msg.sender];
+
+    function withdraw() public {
+        require(balances[msg.sender] > 0);
+        uint256 amount = balances[msg.sender];
         balances[msg.sender] = 0;
         payable(msg.sender).transfer(amount);
     }
 
     function deposit() public payable {
-        if(!exists[msg.sender]){
+        if (!exists[msg.sender]) {
             stakeHolders.push(msg.sender);
             exists[msg.sender] = true;
         }
         balances[msg.sender] += msg.value;
-        totalBalance+= msg.value;
+        totalBalance += msg.value;
     }
 }
